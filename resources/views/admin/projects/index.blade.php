7bd4ef84c9decf1dd9a141f3dc0c80ef35d2f6a3
giordanofabrizii <giordanofabrizi@gmail.com>
2024-08-01 15:05:41 +0200

    changed index of projects

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,23 +1,46 @@
 @extends('layouts.admin')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-12 px-4 py-2 d-flex justify-content-end">
-            <a href="{{ Route('admin.projects.create') }}">
-                <button class="btn btn-primary">New Project</button>
-            </a>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 px-4 py-2 d-flex justify-content-end">
+                <a href="{{ Route('admin.projects.create') }}">
+                    <button class="btn btn-primary">New Project</button>
+                </a>
+            </div>
+        </div>
+
+        <div class="row">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Image Url</th>
+                        <th scope="col">Buttons</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($projects as $project)
+                        <tr>
+                            <th scope="row">{{ $loop->iteration }}</th>
+                            <td>{{ $project->title }}</td>
+                            <td>{{ $project->image }}</td>
+                            <td>
+                                <a class="btn btn-primary" href="{{ Route('admin.projects.show', compact('project')) }}">Show</a>
+                                <a class="btn btn-warning" href="{{route('admin.projects.edit',compact('project'))}}">Edit</a>
+                                <form id="delete-form" class="d-inline" action="{{route('admin.projects.destroy',$project)}}" method="POST" project_title="{{$project->title}}">
+                                    @method('DELETE')
+                                    @csrf
+
+                                    <button class="btn btn-danger" type="submit">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+
+                </tbody>
+            </table>
         </div>
     </div>
-    <div class="row justify-content-center">
-        @foreach ($projects as $project)
-            <article class="col-4">
-                <a href="{{ Route('admin.projects.show', compact('project')) }}">
-                    <img src="{{ $project->image }}" alt="{{ $project->title }} image">
-                    <h1>{{ $project->title }}</h1>
-                </a>
-            </article>
-        @endforeach
-    </div>
-</div>
 @endsection
